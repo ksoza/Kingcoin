f944e94b7a4409c78106b2803d3312b6cf9fb237
ksoza <keisersoza13@gmail.com>
2017-03-10 03:27:58 -0500

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -105,11 +105,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 //TODO Add here your network seed nodes
-const std::initializer_list<const char*> SEED_NODES = {
-  //"your_seed_ip1.com:8080",
-  //"your_seed_ip2.com:8080",
-};
-
+const std::initializer_list<const char*> SEED_NODES = //172.217.6.174:813",
+                                                      //172.217.6.174:81381",};
 struct CheckpointData {
   uint32_t height;
   const char* blockId;
